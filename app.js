f3c134bd9ac87da989d97f63ace455eef586d96b
MEGHANGOWDA <meghangowdath@gmail.com>
2023-12-30 06:27:26 +0530

    my portfolio
    1

---
+++ app.js
@@ -0,0 +1,48 @@
+const hamburgerMenu = document.querySelector('#navigation .nav-icon');
+const navContent = document.querySelector('#nav-content');
+const closeNavButton = document.querySelector('#nav-content .close-btn');
+const navLinks = document.querySelectorAll('#nav-content nav ul li a');
+const scrollButton = document.querySelector(".scroll-top");
+
+// scroll TOP Button Events
+
+if(scrollButton){
+  window.addEventListener('scroll', ()=> {
+    if(pageYOffset > (window.innerHeight * 1.2)){
+      scrollButton.style.display="flex";
+    }else{
+      scrollButton.style.display="none";
+    }
+  });
+  scrollButton.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+  });
+}
+
+// Hamburger Menu events
+hamburgerMenu.addEventListener('click', ()=>{
+  navContent.classList.add('show');
+  document.body.style.overflow="hidden";
+});
+closeNavButton.addEventListener('click', ()=>{
+  navContent.classList.remove('show');
+  document.body.style.overflow="initial";
+});
+navLinks.forEach( link => {
+  link.addEventListener('click', ()=> {
+    navContent.classList.remove('show');
+    document.body.style.overflow="initial";
+  })
+})
+
+function CircleBar(e) {
+  $(e)
+    .circleProgress({ fill: { color: "#00EAFF" } })
+    .on("circle-animation-progress", function(_event, _progress, stepValue) {
+      $(this)
+        .find("strong")
+        .text(String(parseInt(100 * stepValue)) + "%");
+    });
+}
+CircleBar(".round");
+
